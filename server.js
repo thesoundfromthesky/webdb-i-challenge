f8a7e3fa2d33e6aa0dbe0f0f93808115bfc64d00
thesoundfromthesky <nagara@naver.com>
2020-01-20 16:00:49 -0600

    stretch

--- server.js
+++ server.js
@@ -7,14 +7,35 @@ const server = express();
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
+// - Add a `query string` option to your `GET /api/accounts` endpoint.
+// The `query string` may contain `limit`, `sortby` and `sortdir` keys.
+// If these keys are provided, use these values to limit and sort the `accounts`
+// which are selected from the database. Reference the docs for sorting and limiting in `knex`.
+
+// ```js
+// // sample req.query object
+// {
+//   limit: 5,
+//   sortby: 'id',
+//   sortdir: 'desc'
+// }
+// ```
+
 server.get("/api/accounts", async (req, res) => {
   try {
-    const accounts = await db("accounts");
+    const limit = req.query.limit;
+    const sortby = req.query.sortby || "id";
+    const sortdir = req.query.sortdir || "asc";
+    const accounts = await db("accounts")
+      .orderBy(sortby, sortdir)
+      .limit(limit);
+
     res.status(200).json(accounts);
   } catch (err) {
     res.status(500).json({ err });
   }
 });
+
 
 server.get("/api/accounts/:id", async (req, res) => {
   try {
